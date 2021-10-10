f1a5ca963f1ccff0c753b597467962c1127611b1
Roj Serbest <roj@rojser.best>
2021-10-10 15:46:58 +0300

    Use empty file functions for browsers

--- socialvoid/deps.deno.ts
+++ socialvoid/deps.deno.ts
@@ -7,9 +7,13 @@ export const isBrowser = typeof Deno === "undefined";
 
 export const os = isBrowser ? "Browser" : Deno.build.os;
 
-export const writeTextFileSync = Deno.writeTextFileSync;
+export const writeTextFileSync = isBrowser
+  ? (...args: any) => {}
+  : Deno.writeTextFileSync;
 
-export const readTextFileSync = Deno.readTextFileSync;
+export const readTextFileSync = isBrowser
+  ? (...args: any) => ""
+  : Deno.readTextFileSync;
 
 export const getRandomValues = (size: number) => {
   return crypto.getRandomValues(new Uint8Array(size)).buffer;
