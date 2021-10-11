7a8863a489184e31ba1013c93311482942a8ce0b
Roj Serbest <roj@rojser.best>
2021-10-11 11:08:53 +0300

    Create `Date` object for timestamps

--- generators/generateTypes.ts
+++ generators/generateTypes.ts
@@ -3,6 +3,7 @@ import { format } from "./format.ts";
 interface Param {
   type: string;
   description: string;
+  is_date?: boolean;
 }
 
 type Obj = { [key: string]: Param };
@@ -15,6 +16,7 @@ let code = `
 export class TypeBase {
     static fromObject: (obj: any) => TypeBase;
 }
+
 `;
 const toCamel = (s: string) => {
   return s.replace(/([-_][a-z])/gi, ($1) => {
@@ -37,7 +39,9 @@ for (const obj in objs) {
   `;
 
   for (const param in objs[obj]) {
-    code += `public ${toCamel(param)}: ${objs[obj][param].type},`;
+    code += `public ${toCamel(param)}: ${
+      objs[obj][param].is_date ? "Date" : objs[obj][param].type
+    },`;
   }
 
   code += ") {super()}\n\n ";
@@ -45,7 +49,11 @@ for (const obj in objs) {
   code += "static fromObject(obj: any) {";
   code += "return new this(";
   for (const param in objs[obj]) {
-    code += `obj.${param},`;
+    if (objs[obj][param].is_date) {
+      code += `new Date(obj.${param} * 1000)`;
+    } else {
+      code += `obj.${param},`;
+    }
   }
 
   code += ")}}\n\n";
