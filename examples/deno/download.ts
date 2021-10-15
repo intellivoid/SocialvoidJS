7b6569f1b6f9e5e257b239ab6094ac84f23394d0
Roj Serbest <roj@rojser.best>
2021-10-15 13:07:47 +0300

    Update Deno download example

--- examples/deno/download.ts
+++ examples/deno/download.ts
@@ -3,8 +3,7 @@ import { client, login } from "./mod.ts";
 await login();
 
 const id = prompt("ID of the document to be downloaded:")!;
-const data = await client.cdn.download(id);
+const data = (await client.cdn.download(id)) as ArrayBuffer;
 
 await Deno.writeFile(id, new Uint8Array(data));
-
 console.log("Downloaded successfully.");
