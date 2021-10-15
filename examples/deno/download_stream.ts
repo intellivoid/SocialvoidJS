51a50427e7ab9a3203f90662c48549a16992401a
Roj Serbest <roj@rojser.best>
2021-10-15 13:08:06 +0300

    Add download stream example for Deno

---
+++ examples/deno/download_stream.ts
@@ -0,0 +1,13 @@
+import { writableStreamFromWriter } from "https://deno.land/std@0.111.0/streams/conversion.ts";
+import { client, login } from "./mod.ts";
+
+await login();
+
+const id = prompt("ID of the document to be downloaded:")!;
+const result = (await client.cdn.download(id, true)) as Blob;
+
+const writer = await Deno.open(id, { create: true });
+const writeStream = writableStreamFromWriter(writer);
+
+result.stream().pipeTo(writeStream);
+console.log("Downloaded successfully.");
