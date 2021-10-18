6ca934041c12ae1d202b88c51f6764c568d0bcde
Roj Serbest <roj@rojser.best>
2021-10-18 19:35:02 +0300

    Add more network methods
    - followPeer & unfollowPeer

--- socialvoid/methods/network.ts
+++ socialvoid/methods/network.ts
@@ -1,5 +1,5 @@
 import { Request } from "../request.ts";
-import { Peer, Profile } from "../types.ts";
+import { Peer, Profile, RelationshipType } from "../types.ts";
 import { MethodBase } from "./method_base.ts";
 
 export class Network extends MethodBase {
@@ -49,6 +49,30 @@ export class Network extends MethodBase {
                     ? typeof peer == "string" ? peer : peer.id
                     : undefined,
                 cursor,
+            }),
+            true,
+        );
+    }
+
+    /**
+     * Follows another peer on the network.
+     */
+    followPeer(peer: string | Peer): Promise<RelationshipType> {
+        return this.client.invokeRequest(
+            new Request("network.follow_peer", {
+                peer: typeof peer == "string" ? peer : peer.id,
+            }),
+            true,
+        );
+    }
+
+    /**
+     * Unfollows another peer on the network.
+     */
+    unfollowPeer(peer: string | Peer): Promise<RelationshipType> {
+        return this.client.invokeRequest(
+            new Request("network.unfollow_peer", {
+                peer: typeof peer == "string" ? peer : peer.id,
             }),
             true,
         );
