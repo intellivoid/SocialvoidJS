2eb89d0400367813e6e120815afa68bf5b687200
Roj Serbest <roj@rojser.best>
2021-10-21 20:33:13 +0300

    Document parameters of CDN methods

--- socialvoid/methods/cdn.ts
+++ socialvoid/methods/cdn.ts
@@ -3,6 +3,11 @@ import { formFromObj } from "../utils.ts";
 import { MethodBase } from "./method_base.ts";
 
 export class CDN extends MethodBase {
+    /**
+     * Uploads a file.
+     *
+     * @param document The file, it can be a buffer or a readable stream.
+     */
     async upload(document: any): Promise<Document> {
         const form = formFromObj({
             action: "upload",
@@ -13,6 +18,12 @@ export class CDN extends MethodBase {
         return (await this.client.invokeCDNRequest(form)).results;
     }
 
+    /**
+     * Downloads the provided document.
+     *
+     * @param document The ID or instance of the document.
+     * @param stream Wheather to download and returns its data or stream it, returning a `Blob`.
+     */
     async download(document: string | Document, stream?: boolean) {
         const form = formFromObj({
             action: "download",
